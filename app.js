2c93e006433ba4d4e92463a9b8e6c38b660fc06c
felipenehuengutierrez <felipe.nehueng@gmail.com>
2025-10-22 02:47:43 +0000

    Generador Dominio Felipe Gutierrez

---
+++ app.js
@@ -0,0 +1,15 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let a = 0; a <= pronoun.length - 1 ; a++) {
+    for (let b = 0; b <= 1 ; b++){
+        for (let c = 0; c <= 1; c++){
+
+            console.log(pronoun[a],adj[b],noun[c],'.com');
+            
+        }
+
+    }
+
+}
